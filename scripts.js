ec8156dafabaec3353fcb765b062b497fe0d3883
Yosiri <152374704+Yosiri@users.noreply.github.com>
2023-12-13 23:28:20 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -53,3 +53,17 @@ document.getElementById("signup-form").addEventListener("submit", function(event
         alert("Please fill in all the required fields and ensure that the passwords match.");
     }
 });
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
